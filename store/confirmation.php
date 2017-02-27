49fa2aeed669adad58e1787e51ff9bd7d3264663
jandzouana <jandzouana@gmail.com>
2017-02-26 23:02:54 -0800

    started working on store page again

---
+++ store/confirmation.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>EduCamps Inc - Confirmation</title>
+        <link href=../stylesheets/main.css media=screen rel=stylesheet type=text/css />
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+				<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+    </head>
+    <body>
+        <div class="main_container" id="store_container">
+          <div id=menubar>
+            <a href="../">
+							<img id="logo" src="../icons/logo.svg" alt="EduCamps logo" />
+            </a>
+            <table>
+                <tr>
+                    <td><a href=../schedule>Schedule</a></td>
+                    <td><a href=../registration>Registration</a></td>
+                    <td><a href=../store>Store</a></td>
+                    <td><a href=../gallery>Gallery</a></td>
+                    <td><a href=../forum>Forum</a></td>
+                    <td><a href=../data>Data</a></td>
+                    <td><a href=../activities>Activities</a></td>
+                </tr>
+            </table>
+          </div>
+          <div id="conf_main_content" class = "main_content">
+
+					</div>
+        </div>
+    </body>
+</html>
