6c1e122c9a86d9908adac2875f93ee479ee0c891
waltertliu <walter@MacBook-Air.local>
2017-02-26 18:00:10 -0800

    fixed registration confirmation page

--- registration/registration.php
+++ registration/registration.php
@@ -4,32 +4,52 @@
         <title>Your Camp Order</title>
         <?php
             $cost = $_REQUEST[duration]*50;
-            $icon = "smiley.png";
-            if($_REQUEST[email]=="acarvalhogoncalves@scu.edu")
-                $icon = "eric.jpg";
         ?>
         <script src=pay.js type="text/javascript"></script>
         <link href=../stylesheets/main.css media=screen rel=stylesheet type=text/css />
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
     </head>
     <body>
-        <div id=menubar>
-						<a href="../">
-            	<img src="../<?= $icon ?>" alt="EduCamps logo" />
-						</a>
-            <table>
-                <tr>
-                    <td><a href=schedule.html>Schedule</a></td>
-                    <td><a href=registration.html>Registration</a></td>
-                    <td><a href=store.html>Store</a></td>
-                    <td><a href=gallery.html>Gallery</a></td>
-                    <td><a href=forum.html>Forum</a></td>
-                    <td><a href=data.html>Data</a></td>
-                    <td><a href=activities.html>Activities</a></td>
-                </tr>
-            </table>
+        <div class=main_container>
+            <div id=menubar>
+                <a href="../">
+									<img id="logo" src="../icons/logo.svg" alt="EduCamps logo" />
+                </a>
+                <table>
+                    <tr>
+                        <td><a href=../schedule>Schedule</a></td>
+                        <td><a href=../registration>Registration</a></td>
+                        <td><a href=../store>Store</a></td>
+                        <td><a href=../gallery>Gallery</a></td>
+                        <td><a href=../forum>Forum</a></td>
+                        <td><a href=../data>Data</a></td>
+                        <td><a href=../activities>Activities</a></td>
+                    </tr>
+                </table>
+            </div>
+            <div class = "main_content">
+                <p>Your Camp Order: $<?= $cost ?></p>
+                <img src="../icons/PayPal.png" alt="Pay with PayPal" id="pay" />
+            </div>
+            <div id=footer>
+            <div id="left-footer">
+                <img id="logo" src="../icons/logo.svg" alt="EduCamps logo" />
+                <a href=webmaster.html>Webmaster</a>
+                <a href=contact.html>Contact Us</a>
+            </div>
+                <table id="right-footer">
+                    <tr>
+                        <td>
+                            <a href="https://www.facebook.com"><img src=../icons/facebook.svg alt="facebook icon"/></a>
+                            <a href="https://www.twitter.com"><img src=../icons/twitter.svg alt="twitter icon"/></a>
+                            <a href="https://www.instagram.com"><img src=../icons/instagram.svg alt="instagram icon"/></a>
+                            <a href="https://www.snapchat.com"><img src=../icons/snapchat.svg alt="snapchat icon"/></a>
+                            <br/>
+                        </td>
+                    </tr>
+                </table>
+            </div>
         </div>
-        <p>Your Camp Order: $<?= $cost ?>
-        </p>
-        <button id=pay>Pay With (PayPal)</button>
+        
     </body>
 </html>
