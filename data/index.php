ae47ca0182704a8042dff34fd7aab399c427759c
s-topp <sam.topp@icloud.com>
2017-03-12 21:21:53 -0700

    got rid of the error checking on data page

--- data/index.php
+++ data/index.php
@@ -174,22 +174,3 @@ while($camp_name = mysqli_fetch_array($namequery))
             </div>
     </body>
 </html>
-
-/*
-  dataPoints: [
- 	{ label: "Apache",  y: 10  },
- 	{ label: "Eureka",  y: 10  },
- 	{ label: "Santa Clara", y: 15  },
- 	{ label: "Tahoe South", y: 25  },
- 	{ label: "Boulder",  y: 30  },
- 	{ label: "Chesapeake",  y: 28  },
- 	{ label: "Grapfruit Resort",  y: 10  },
- 	{ label: "Grapfruit Resort 2", y: 15  },
- 	{ label: "Little Derby", y: 25  },
- 	{ label: "Sweet Bayou",  y: 30  },
- 	{ label: "Nestor Camp",  y: 28  },
- 	{ label: "Lewis and Clark",  y: 10  },
- 	{ label: "Robotics Village", y: 15  },
- 	{ label: "Salt Camp", y: 25  },
- 	{ label: "Federal Camp",  y: 30  },
- ]*/
